3c505a90bd3aa49fcad31574ee582c53998327ae
Ayushi <ayumak33@gmail.com>
2026-08-03 07:54:24 +0530

    Dataset and DataLoader

--- train.py
+++ train.py
@@ -29,3 +29,40 @@ print(label)
 plt.imshow(image.squeeze(), cmap="gray")
 plt.title(f"Label: {label}")
 plt.show()
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=64,
+    shuffle=False
+)
+
+images, labels = next(iter(train_loader))
+
+print(images.shape)
+print(labels.shape)
+
+image = images[0]
+print(image.shape)
+print(labels[0])
+
+# display first image
+plt.imshow(images[0].squeeze(), cmap="gray")
+plt.title(f"Label: {labels[0].item()}")
+plt.show()
+
+# display first 10 images
+fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(images[i].squeeze(), cmap="gray")
+    ax.set_title(labels[i].item())
+    ax.axis("off")
+
+plt.tight_layout()
+plt.show()
