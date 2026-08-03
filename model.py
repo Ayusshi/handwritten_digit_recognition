6298e9cdf3477f56c8062d58df63b23251ffa939
Ayushi <ayushi.m@techovarya.com>
2026-08-03 11:38:30 +0530

    bug fix for python version and exected train.py

--- model.py
+++ model.py
@@ -36,3 +36,25 @@ class CNN(nn.Module):
             128,
             10
         )
+
+def forward(self, x):
+
+    x = self.conv1(x)
+    x = self.relu1(x)
+    x = self.pool1(x)
+
+    x = self.conv2(x)
+    x = self.relu2(x)
+    x = self.pool2(x)
+
+    x = self.flatten(x)
+
+    x = self.fc1(x)
+    x = self.relu3(x)
+
+    x = self.fc2(x)
+
+    return x
+
+model = CNN()
+print(model)
